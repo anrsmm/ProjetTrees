c70a0d789fe9cc6175810483d6808805f55f63c4
anrsmm <aroques17@gmail.com>
2026-04-09 00:10:59 +0200

    correction fonction creer_grille dans grille.c

--- grille.c
+++ grille.c
@@ -6,26 +6,98 @@
 
 
 // création grille
-Grille*creer_grille(int Hauteur, int Largeur){
-	Grille *g;
+Grille *creer_grille(int Hauteur, int Largeur) {
+    Grille *g;
+    int i, j;
 
-	if (Hauteur <= 0 || Largeur <=0) {
-		return NULL;
-	}
+    if (Hauteur <= 0 || Largeur <= 0) {
+        return NULL;
+    }
 
-	g = malloc(sizeof(Grille));
-	if (g == NULL) {
-		return NULL;
-	}
+    g = malloc(sizeof(Grille));
+    if (g == NULL) {
+        return NULL;
+    }
 
-	g->Hauteur = Hauteur;
-	g->Largeur = Largeur;
-	//initialiser tree_nbr et empty_nbr
-	g->tree_nbr = 0;
-	g->empty_nbr = 0;
+    g->Hauteur = Hauteur;
+    g->Largeur = Largeur;
+    g->tree_nbr = 0;
+    g->empty_nbr = 0;
 
-	return g;
+    g->ligne_nbr = malloc(Hauteur * sizeof(int));
+    if (g->ligne_nbr == NULL) {
+        free(g);
+        return NULL;
+    }
+
+    g->col_nbr = malloc(Largeur * sizeof(int));
+    if (g->col_nbr == NULL) {
+        free(g->ligne_nbr);
+        free(g);
+        return NULL;
+    }
+
+    for (i = 0; i < Hauteur; i++) {
+        g->ligne_nbr[i] = 0;
+    }
+
+    for (i = 0; i < Largeur; i++) {
+        g->col_nbr[i] = 0;
+    }
+
+    g->trees = malloc(Hauteur * Largeur * sizeof(Position));
+    if (g->trees == NULL) {
+        free(g->col_nbr);
+        free(g->ligne_nbr);
+        free(g);
+        return NULL;
+    }
+
+    g->empty_cases = malloc(Hauteur * Largeur * sizeof(Position));
+    if (g->empty_cases == NULL) {
+        free(g->trees);
+        free(g->col_nbr);
+        free(g->ligne_nbr);
+        free(g);
+        return NULL;
+    }
+
+    g->est_tree = malloc(Hauteur * sizeof(int *));
+    if (g->est_tree == NULL) {
+        free(g->empty_cases);
+        free(g->trees);
+        free(g->col_nbr);
+        free(g->ligne_nbr);
+        free(g);
+        return NULL;
+    }
+
+    for (i = 0; i < Hauteur; i++) {
+        g->est_tree[i] = malloc(Largeur * sizeof(int));
+        if (g->est_tree[i] == NULL) {
+            for (j = 0; j < i; j++) {
+                free(g->est_tree[j]);
+            }
+            free(g->est_tree);
+            free(g->empty_cases);
+            free(g->trees);
+            free(g->col_nbr);
+            free(g->ligne_nbr);
+            free(g);
+            return NULL;
+        }
+    }
+
+    for (i = 0; i < Hauteur; i++) {
+        for (j = 0; j < Largeur; j++) {
+            g->est_tree[i][j] = 0;
+        }
+    }
+
+    return g;
 }
+
+
 
 // destruction grille
 void free_grille(Grille*g){
